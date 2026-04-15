af8c3ac6b3ba40946cbd41e73422ae037b6fbaa1
siama-ismail <acayosiamaismail@gmail.com>
2026-04-15 21:50:51 +0300

    the two mehtods

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function cleanDatabase(recordIds) {
-    // Requirement: Remove all odd numbers from the array
-    // FIX: Instead of splicing while looping (which skips items),
-    // I build a new array with only even numbers.
-    return recordIds.filter(id => id % 2 === 0);
-}
+// function cleanDatabase(recordIds) {
+// // Requirement: Remove all odd numbers from the array
+// for (let i = 0; i < recordIds.length; i++) {
+// if (recordIds[i] % 2 !== 0) {
+// recordIds.splice(i, 1);
+// i--;
+// }
+// }
+// return recordIds;
+// }
+// // Test Case
+// const data = [1, 3, 4, 6, 7, 9, 10];
+// const cleaned = cleanDatabase(data);
+// console.log(cleaned)
 
+
+
+function cleanDatabase(recordIds) {
+  const cleaned =[]
+for (let i = 0; i < recordIds.length; i++) {
+if (recordIds[i] % 2 !== 0) {
+cleaned.push(recordIds[i]);
+}
+}
+return cleaned;
+}
 // Test Case
 const data = [1, 3, 4, 6, 7, 9, 10];
 const cleaned = cleanDatabase(data);
-console.log("Final List:", cleaned); 
+console.log(cleaned)
